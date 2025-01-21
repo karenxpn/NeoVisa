e5dbfcc6db77c399b8efe74040bfd2a5a5bfa436
karenxpn <karenmirakyan@gmail.com>
2025-01-21 11:10:49 +0400

    setup database

--- jwt_token.py
+++ jwt_token.py
@@ -5,6 +5,7 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt, ExpiredSignatureError
 from starlette import status
 
+from database import get_db
 from dotenv import load_dotenv
 
 load_dotenv()
