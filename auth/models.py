1d133efd2842fcb2acf04be8977a5b6894141f62
karenxpn <karenmirakyan@gmail.com>
2025-01-21 12:30:24 +0400

    authentication models separated

--- auth/models.py
+++ auth/models.py
@@ -4,13 +4,17 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime
 
 from database import Base
 
+class PhoneOtp(Base):
+    __tablename__ = "phone_otps"
+    id = Column(Integer, primary_key=True, index=True)
+    phone_number = Column(String, index=True, unique=True)
+    otp = Column(String, index=True)
+    created_at = Column(DateTime, default=datetime.now(tz=timezone.utc))
 
-class User(Base):
-    __tablename__ = "users"
+
+class Token(Base):
+    __tablename__ = "tokens"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
-    phone_number = Column(String, index=True, unique=True)
-    is_verified = Column(Boolean, default=False)
-    created_at = Column(DateTime, default=datetime.now(tz=timezone.utc))
-    updated_at = Column(DateTime, default=datetime.now(tz=timezone.utc), onupdate=datetime.now(tz=timezone.utc))
+    user_id = Column(Integer, ForeignKey("users.id"), index=True, nullable=False)
+    token = Column(String, index=True)
