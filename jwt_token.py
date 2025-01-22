6ec39b8d00cff12e75278bc9d44f853dacbe9d86
karenxpn <karenmirakyan@gmail.com>
2025-01-22 12:02:35 +0400

    extract user from token

--- jwt_token.py
+++ jwt_token.py
@@ -3,10 +3,14 @@ from datetime import timedelta, datetime, timezone
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt, ExpiredSignatureError
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from database import get_db
 from dotenv import load_dotenv
+
+from user.models import User
 
 load_dotenv()
 
@@ -39,3 +43,29 @@ def decode_access_token(token: str):
     except JWTError as e:
         print(e)
         return None
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme),
+                           db: AsyncSession = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        phone: str = payload.get("sub")
+        if phone is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    result = await db.execute(
+        select(User).where(User.phone_number==phone)
+    )
+
+    user = result.scalar_one_or_none()
+    if user is None:
+        raise credentials_exception
+    return user
