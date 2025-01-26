4a6751f47f2e91d12309d946ee1459fa0317a8f4
karenxpn <karenmirakyan@gmail.com>
2025-01-26 21:26:54 +0400

    pydantic v2

--- user/requests.py
+++ user/requests.py
@@ -1,22 +1,19 @@
 from datetime import date
-from typing import Optional
 
-from pydantic import BaseModel, EmailStr
-from pydantic.v1 import constr
-
+from pydantic import BaseModel, EmailStr, Field
 
 class UpdateUserRequest(BaseModel):
     email: EmailStr
 
     # passport info
-    passport_number: constr(min_length=6, max_length=20, regex=r'^[A-Za-z0-9]+$')
-    passport_type: constr(min_length=1)
-    issuer_country: constr(min_length=1)
+    passport_number: str = Field(..., min_length=6, max_length=20, pattern=r'^[A-Za-z0-9]+$')
+    passport_type: str = Field(..., min_length=1)
+    issuer_country: str = Field(..., min_length=1)
     issue_date: date
     expire_date: date
-    issue_place: constr(min_length=1)
+    issue_place: str = Field(..., min_length=1)
 
     # user info
-    first_name: constr(min_length=1, regex=r'^[a-zA-Z\s]+$')
-    last_name: Optional[constr(min_length=1, regex=r'^[a-zA-Z\s]+$')] = None
-    family_name: constr(min_length=1)
+    first_name: str = Field(..., min_length=1, pattern=r'^[a-zA-Z\s]+$')
+    last_name: str = Field(None, min_length=1, pattern=r'^[a-zA-Z\s]+$')
+    family_name: str = Field(..., min_length=1)
