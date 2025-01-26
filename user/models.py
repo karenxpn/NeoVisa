cc91d51de8eed5e39d99fe0f9e10d96e826f069b
karenxpn <karenmirakyan@gmail.com>
2025-01-26 13:00:58 +0400

    passport type

--- user/models.py
+++ user/models.py
@@ -1,8 +1,12 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import Relationship
 
 from core.database import Base
+from enum import Enum as PyEnum
+from sqlalchemy import Enum
+
 
 
 class User(Base):
@@ -14,3 +18,31 @@ class User(Base):
     is_verified = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.now(tz=timezone.utc))
     updated_at = Column(DateTime, default=datetime.now(tz=timezone.utc), onupdate=datetime.now(tz=timezone.utc))
+
+    passport = Relationship('Passport', back_populates='user', passive_deletes=True)
+
+
+class PassportType(PyEnum):
+    ORDINARY = "Ordinary"
+    DIPLOMATIC = "Diplomatic"
+    COLLECTIVE = "Collective"
+    SERVICE = "Service"
+    OFFICIAL = "Official"
+    FOREIGNERS = 'Passport of foreigners'
+    PROTECTION = "Protection passport"
+    UN = 'UN laissez-passer'
+
+
+class Passport(Base):
+    __tablename__ = "passports"
+    id = Column(Integer, primary_key=True, index=True)
+    passport_number = Column(String, unique=True, index=True)
+    passport_type = Column(Enum(PassportType), nullable=False, index=True)
+    issuer_country = Column(String, index=True)
+    issue_date = Column(DateTime, default=datetime.now(tz=timezone.utc))
+    expire_date = Column(DateTime, default=datetime.now(tz=timezone.utc))
+    issue_place = Column(String, index=True)
+
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False, index=True, unique=True)
+
+    user = Relationship('User', back_populates="passports")
