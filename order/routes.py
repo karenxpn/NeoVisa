fb9cf9064a6bd398c8048558c93a36d117f23273
karenxpn <karenmirakyan@gmail.com>
2025-02-04 21:50:39 +0400

    get single order by id

--- order/routes.py
+++ order/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.functions import current_user
 
 from core.database import get_db
 from core.jwt_token import get_current_user
@@ -11,6 +12,14 @@ router = APIRouter()
 
 @router.post("/orders")
 async def create_order(data: CreateOrderRequest,
-                       current_user: User = Depends(get_current_user),
+                       user: User = Depends(get_current_user),
                        db: AsyncSession = Depends(get_db)):
-    return await OrderService.create_order(db, current_user, data)
+    return await OrderService.create_order(db, user, data)
+
+
+@router.get("/orders/{order_id}")
+async def get_order(order_id: int,
+                    user: User = Depends(get_current_user),
+                    db: AsyncSession = Depends(get_db)):
+
+    return await OrderService.get_order(order_id, db, user)
--- order/services.py
+++ order/services.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.proceed_request import proceed_request
@@ -21,3 +23,17 @@ class OrderService:
                 'success': True,
                 'message': 'Order created',
             }
+
+    @staticmethod
+    async def get_order(order_id: int, db: AsyncSession, user: User):
+        async with proceed_request(db) as db:
+            order = await db.execute(
+                select(Order).where(Order.id == order_id, Order.user_id == user.id)
+            )
+
+            order = order.scalars().first()
+
+            if order:
+                return order
+
+            raise HTTPException(status_code=404, detail="Order not found")
