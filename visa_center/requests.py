d50967c6635abe8b6bccb4034318c3266fc98927
karenxpn <karenmirakyan@gmail.com>
2025-02-12 19:52:36 +0400

    add visa account passport request model

--- visa_center/requests.py
+++ visa_center/requests.py
@@ -1,5 +1,5 @@
+from datetime import date
 from pydantic import BaseModel, Field
-
 from visa_center.models.visa_center_model import CountryEnum
 
 
@@ -7,3 +7,17 @@ class AddVisaAccountRequest(BaseModel):
     username: str = Field(..., min_length=3, max_length=50)
     password: str = Field(..., min_length=8)
     country: CountryEnum = Field(...)
+
+
+class AddVisaAccountPassport(BaseModel):
+    passport_number: str = Field(min_length=6, max_length=20, pattern=r'^[A-Za-z0-9]+$')
+    passport_type: str = Field(min_length=1)
+    issuer_country: str = Field(min_length=1)
+    issue_date: date
+    expire_date: date
+    issue_place: str = Field(None, min_length=1)
+
+    name: str = Field(min_length=1)
+    surname: str = Field(min_length=1)
+    nationality: str = Field(min_length=1)
+
